1a5a3a5b1f36a1f8df5da54cc60be7c645759fcc
liqiongtao <hnatao@126.com>
2021-10-25 17:39:32 +0800

    bugfix

--- goo-grpc/context.go
+++ goo-grpc/context.go
@@ -15,7 +15,7 @@ func Context(c *gin.Context) context.Context {
 	ctx, _ := context.WithTimeout(context.Background(), 8*time.Second)
 	md := metadata.New(map[string]string{})
 	if c != nil {
-		md.Set("request-id", fmt.Sprintf("%d", c.GetInt("__trace_id")))
+		md.Set("request-id", fmt.Sprintf("%d", c.GetInt("__request_id")))
 		md.Set("request-server", c.GetString("__server_name"))
 		files := goo_utils.Trace(2)
 		if trimDir := path.Dir(c.GetString("__base_dir")); trimDir != "" {
