29297a140bb489097c40bfbf4b5550e3c605918d
liqiongtao <hnatao@126.com>
2022-02-05 13:59:31 +0800

    bugfix

--- goo-utils/string.go
+++ goo-utils/string.go
@@ -1,7 +1,9 @@
 package goo_utils
 
 import (
+	"bytes"
 	"strings"
+	"unicode"
 )
 
 // 多字符切割，默认支持逗号，分号，\n
@@ -14,4 +16,29 @@ func Split(s string, rs ...rune) []string {
 		}
 		return r == ',' || r == '，' || r == ';' || r == '；' || r == '\n'
 	})
+}
+
+// 驼峰转下划线
+func Camel2Case(str string) string {
+	var bf bytes.Buffer
+
+	for i, r := range str {
+		if !unicode.IsUpper(r) {
+			bf.WriteRune(r)
+			continue
+		}
+		if i > 0 {
+			bf.WriteString("_")
+		}
+		bf.WriteRune(unicode.ToLower(r))
+	}
+
+	return bf.String()
+}
+
+// 下划线转驼峰
+func Case2Camel(str string) string {
+	str = strings.Replace(str, "_", " ", -1)
+	str = strings.Title(str)
+	return strings.Replace(str, " ", "", -1)
 }
