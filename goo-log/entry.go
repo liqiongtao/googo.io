1501173b230a2cfa6bd57f8eb5e73d742e60a88d
liqiongtao <hnatao@126.com>
2022-02-23 15:05:25 +0800

    bugfix

--- goo-log/entry.go
+++ goo-log/entry.go
@@ -121,6 +121,7 @@ func (entry *Entry) trace(n int) (arr []string) {
 		if file == "" {
 			continue
 		}
+		fmt.Println(file, line)
 		if index := strings.Index(file, "googo.io"); index != -1 {
 			file = file[index:]
 		}
