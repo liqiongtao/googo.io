a7cba6747578dc3786425c90e6f1a0df439b074d
liqiongtao <hnatao@126.com>
2022-04-06 19:39:33 +0800

    bugfix

--- goo-utils/captcha.go
+++ goo-utils/captcha.go
@@ -2,12 +2,21 @@ package goo_utils
 
 import (
 	"github.com/mojocn/base64Captcha"
+	"sync"
 )
 
-var _store = base64Captcha.DefaultMemStore
+var (
+	__store     base64Captcha.Store
+	__storeOnce sync.Once
+)
 
 // 获取图片验证码
 func CaptchaGet(width, height int) map[string]string {
+	// 初始化
+	__storeOnce.Do(func() {
+		__store = base64Captcha.DefaultMemStore
+	})
+
 	if width == 0 {
 		width = 240
 	}
@@ -22,7 +31,7 @@ func CaptchaGet(width, height int) map[string]string {
 		MaxSkew:  0.7,
 		DotCount: 80,
 	}
-	ca := base64Captcha.NewCaptcha(&driver, _store)
+	ca := base64Captcha.NewCaptcha(&driver, __store)
 	id, b64s, _ := ca.Generate()
 
 	return map[string]string{
@@ -33,5 +42,8 @@ func CaptchaGet(width, height int) map[string]string {
 
 // 验证图片验证码
 func CaptchaVerify(id, code string) bool {
-	return _store.Verify(id, code, true)
+	if __store.Verify == nil {
+		return false
+	}
+	return __store.Verify(id, code, true)
 }
