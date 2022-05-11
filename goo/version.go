cfaa64ea6c837e68392001b336284d09ec55b356
liqiongtao <hnatao@126.com>
2022-05-11 14:26:38 +0800

    bugfix

--- goo/version.go
+++ goo/version.go
@@ -4,6 +4,30 @@ import (
 	"flag"
 )
 
+/**
+  - flag.go
+
+	func init() {
+		if *goo.VersionFlag {
+			fmt.Println(goo.Version)
+			os.Exit(0)
+		}
+	}
+
+  - deploy.sh
+
+	// 定义
+	version=v1.0.1
+	buildVersion="${version}.$(date +%Y%m%d).$(date +%H%M)"
+
+	// 编译
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X goo.Version=$buildVersion" -o ss
+
+  - 执行
+
+	./ss -v
+*/
+
 var (
 	Version     string
 	VersionFlag = flag.Bool("v", false, "version")
