660f2b9e14e0084c0ea43ea7df1226d76c798f02
liqiongtao <hnatao@126.com>
2022-06-09 21:18:07 +0800

    bugfix

--- goo/env.go
+++ goo/env.go
@@ -3,12 +3,25 @@ package goo
 type Env string
 
 const (
-	PRODUCTION  Env = "prod"
+	PRODUCTION  Env = "production"
 	SIM         Env = "sim"
 	TEST        Env = "test"
-	DEVELOPMENT Env = "dev"
+	DEVELOPMENT Env = "development"
+)
+
+var (
+	envTags = map[Env]string{
+		PRODUCTION:  "prod",
+		SIM:         "sim",
+		TEST:        "test",
+		DEVELOPMENT: "dev",
+	}
 )
 
 func (env Env) String() string {
 	return string(env)
+}
+
+func (env Env) Tag() string {
+	return envTags[env]
 }
