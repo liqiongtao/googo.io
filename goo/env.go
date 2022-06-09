4e4d3a8fd6e832709aa678210923746b8c76d52e
liqiongtao <hnatao@126.com>
2022-06-09 21:16:00 +0800

    bugfix

--- goo/env.go
+++ goo/env.go
@@ -3,10 +3,10 @@ package goo
 type Env string
 
 const (
-	PRODUCTION  Env = "production"
+	PRODUCTION  Env = "prod"
 	SIM         Env = "sim"
 	TEST        Env = "test"
-	DEVELOPMENT Env = "development"
+	DEVELOPMENT Env = "dev"
 )
 
 func (env Env) String() string {
