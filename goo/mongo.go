266a6a92feae25a86074c4db8aaf8afb0fbd31b6
liqiongtao <hnatao@126.com>
2022-07-05 12:59:58 +0800

    bugfix

--- goo/mongo.go
+++ goo/mongo.go
@@ -1,7 +1,10 @@
 package goo
 
-import goo_mongo "github.com/liqiongtao/googo.io/goo-mongo"
+import (
+	goo_mongo "github.com/liqiongtao/googo.io/goo-mongo"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-func Mongo(names ...string) *goo_mongo.Client {
-	return goo_mongo.GetClient(names...)
+func Mongo(names ...string) *mongo.Database {
+	return goo_mongo.GetClient(names...).DB()
 }
