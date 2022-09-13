632107355f545a3b1eaea385712eb10d0b27aeda
liqiongtao <hnatao@126.com>
2022-09-13 16:28:16 +0800

    bugfix

--- goo-grpc/client.go
+++ goo-grpc/client.go
@@ -1,6 +1,7 @@
 package goo_grpc
 
 import (
+	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 	"time"
@@ -18,4 +19,18 @@ func Dial(addr string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 		grpc.WithChainStreamInterceptor(clientStreamInterceptorLog()),
 	)
 	return grpc.Dial(addr, opts...)
+}
+
+func DialContext(ctx context.Context, addr string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	opts = append(opts,
+		grpc.WithInsecure(),
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:                10 * time.Second,
+			Timeout:             100 * time.Millisecond,
+			PermitWithoutStream: true,
+		}),
+		grpc.WithChainUnaryInterceptor(clientUnaryInterceptorLog()),
+		grpc.WithChainStreamInterceptor(clientStreamInterceptorLog()),
+	)
+	return grpc.DialContext(ctx, addr, opts...)
 }
--- goo-grpc/client_etcd.go
+++ goo-grpc/client_etcd.go
@@ -1,6 +1,7 @@
 package goo_grpc
 
 import (
+	"context"
 	"fmt"
 	goo_etcd "github.com/liqiongtao/googo.io/goo-etcd"
 	"go.etcd.io/etcd/client/v3/naming/resolver"
@@ -30,4 +31,26 @@ func DialWithEtcd(serviceName string, cli *goo_etcd.Client) (*grpc.ClientConn, e
 	}
 
 	return grpc.Dial(builder.Scheme()+":///"+serviceName, opts...)
+}
+
+func DialContextWithEtcd(ctx context.Context, serviceName string, cli *goo_etcd.Client) (*grpc.ClientConn, error) {
+	builder, err := resolver.NewBuilder(cli.Client)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithResolvers(builder),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)),
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:                10 * time.Second,
+			Timeout:             100 * time.Millisecond,
+			PermitWithoutStream: true,
+		}),
+		grpc.WithChainUnaryInterceptor(clientUnaryInterceptorLog()),
+		grpc.WithChainStreamInterceptor(clientStreamInterceptorLog()),
+	}
+
+	return grpc.DialContext(ctx, builder.Scheme()+":///"+serviceName, opts...)
 }
