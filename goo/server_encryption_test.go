1ba02ae37fe7cbd6d1173a88913d0b036acb0df7
liqiongtao <hnatao@126.com>
2023-04-22 16:12:52 +0800

    bugix

--- goo/server_encryption_test.go
+++ goo/server_encryption_test.go
@@ -5,30 +5,32 @@ import (
 	"testing"
 )
 
+func TestEncryption_Encode(t *testing.T) {
+	enc := &Encryption{
+		Key:    "4c98542af9fd65fc",
+		Secret: "12fa1e087ab15eba558e12ea64d0f3f8",
+	}
+
+	str, err := enc.Encode([]byte("123adbasdf"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println(str)
+}
+
 func TestEncryption_Decode(t *testing.T) {
 	enc := &Encryption{
-		Key:    "1a63a991a95bea49e35e0c6ab1da66fd",
-		Secret: "1a6395991ae3bea49ab1da65e0c6a6fd",
+		Key:    "4c98542af9fd65fc",
+		Secret: "12fa1e087ab15eba558e12ea64d0f3f8",
 	}
 
-	str, err := enc.Encode([]byte(""))
+	b, err := enc.Decode("a12241192af531a8361b9d195bb9a7863ae5a9507bbb3ad589b6f18293e18509df3bd1174e8eea1afc6ac73d1b70ff26")
 	if err != nil {
-		log.Println(err)
+		log.Println(err.Error())
 		return
 	}
 
-	log.Println(">>1>>", str)
-
-	enc2 := &Encryption{
-		Key:    "29a9916a6395bea4aae35e0cb1da66fd",
-		Secret: "2a6e35e0c69a991a395bea4ab1da66fd",
-	}
-
-	b, err := enc2.Decode(str)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	log.Println(">>2>>", string(b))
+	log.Println(string(b))
 }
