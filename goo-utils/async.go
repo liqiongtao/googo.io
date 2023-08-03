1f133ff12432f486dd491cf9330703b7dfcdcba0
liqiongtao <hnatao@126.com>
2023-08-03 12:11:19 +0800

    优化AsyncFuncGroup

--- goo-utils/async.go
+++ goo-utils/async.go
@@ -2,6 +2,7 @@ package goo_utils
 
 import (
 	goo_log "github.com/liqiongtao/googo.io/goo-log"
+	"runtime"
 	"sync"
 )
 
@@ -22,13 +23,19 @@ func AsyncFunc(fn func()) {
 
 // 异步并发执行（安全）
 func AsyncFuncGroup(fns ...func()) {
-	var wg sync.WaitGroup
+	var (
+		wg sync.WaitGroup
+		ch = make(chan struct{}, runtime.NumCPU())
+	)
 
 	for _, fn := range fns {
 		wg.Add(1)
+		ch <- struct{}{}
+
 		func(fn func()) {
 			AsyncFunc(func() {
 				defer wg.Done()
+				defer func() { <-ch }()
 				fn()
 			})
 		}(fn)
