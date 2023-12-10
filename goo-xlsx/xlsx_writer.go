46845bc990d184a375091f473ff021fbffc0f064
liqiongtao <hnatao@126.com>
2023-12-10 19:15:39 +0800

    bufix

--- goo-xlsx/xlsx_writer.go
+++ goo-xlsx/xlsx_writer.go
@@ -11,33 +11,47 @@ func Writer() *xlsxWrite {
 	return &xlsxWrite{
 		fh:        excelize.NewFile(),
 		sheetName: "Sheet1",
+		sheetRowNums: map[string]int{
+			"Sheet1": 0,
+		},
 	}
 }
 
 type xlsxWrite struct {
-	fh        *excelize.File
-	sheetName string
-	titles    *[]string
-	rows      []*[]interface{}
+	fh           *excelize.File
+	sheetName    string
+	sheetRowNums map[string]int
 }
 
 func (x *xlsxWrite) Handler() *excelize.File {
 	return x.fh
 }
 
-func (x *xlsxWrite) SetTitles(titles []string) *xlsxWrite {
-	x.titles = &titles
-	return x
+func (x *xlsxWrite) SetTitles(titles []string) error {
+	x.sheetRowNums[x.sheetName]++
+	if err := x.fh.SetSheetRow(x.sheetName, fmt.Sprintf("A%d", x.sheetRowNums[x.sheetName]), titles); err != nil {
+		goo_log.Error(err)
+		return err
+	}
+	return nil
 }
 
-func (x *xlsxWrite) SetData(data []interface{}) *xlsxWrite {
-	x.rows = append(x.rows, &data)
-	return x
+func (x *xlsxWrite) SetData(data []interface{}) error {
+	x.sheetRowNums[x.sheetName]++
+	if err := x.fh.SetSheetRow(x.sheetName, fmt.Sprintf("A%d", x.sheetRowNums[x.sheetName]), data); err != nil {
+		goo_log.Error(err)
+		return err
+	}
+	return nil
 }
 
 func (x *xlsxWrite) SetRows(data [][]interface{}) *xlsxWrite {
 	for _, i := range data {
-		x.rows = append(x.rows, &i)
+		x.sheetRowNums[x.sheetName]++
+		if err := x.fh.SetSheetRow(x.sheetName, fmt.Sprintf("A%d", x.sheetRowNums[x.sheetName]), i); err != nil {
+			goo_log.Error(err)
+			continue
+		}
 	}
 	return x
 }
@@ -45,22 +59,11 @@ func (x *xlsxWrite) SetRows(data [][]interface{}) *xlsxWrite {
 func (x *xlsxWrite) SetSheetName(sheetName string) *xlsxWrite {
 	x.fh.NewSheet(sheetName)
 	x.sheetName = sheetName
+	x.sheetRowNums[sheetName] = 0
 	return x
 }
 
 func (x *xlsxWrite) Save2File(filename string) (err error) {
-	if err = x.fh.SetSheetRow(x.sheetName, "A1", x.titles); err != nil {
-		goo_log.Error(err)
-		return
-	}
-
-	for i := 0; i < len(x.rows); i++ {
-		if err = x.fh.SetSheetRow("Sheet1", fmt.Sprintf("A%d", i+2), x.rows[i]); err != nil {
-			goo_log.Error(err)
-			return
-		}
-	}
-
 	if err = x.fh.SaveAs(filename); err != nil {
 		goo_log.Error(err)
 		return
@@ -70,18 +73,6 @@ func (x *xlsxWrite) Save2File(filename string) (err error) {
 }
 
 func (x *xlsxWrite) Output(ctx *gin.Context, filename string) (err error) {
-	if err = x.fh.SetSheetRow(x.sheetName, "A1", x.titles); err != nil {
-		goo_log.Error(err)
-		return
-	}
-
-	for i := 0; i < len(x.rows); i++ {
-		if err = x.fh.SetSheetRow("Sheet1", fmt.Sprintf("A%d", i+2), x.rows[i]); err != nil {
-			goo_log.Error(err)
-			return
-		}
-	}
-
 	ctx.Header("Content-Type", "application/octet-stream")
 	ctx.Header("Content-Disposition", "attachment; filename="+filename)
 	ctx.Header("Content-Transfer-Encoding", "binary")
