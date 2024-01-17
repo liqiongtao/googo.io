643a6fc74501247080e2be72af73f67677f3b506
liqiongtao <hnatao@126.com>
2024-01-17 20:33:17 +0800

    bugfix

--- goo-cron/cron.go
+++ goo-cron/cron.go
@@ -2,40 +2,42 @@ package goo_cron
 
 import (
 	"fmt"
+	goo_context "github.com/liqiongtao/googo.io/goo-context"
+	goo_log "github.com/liqiongtao/googo.io/goo-log"
 	"github.com/robfig/cron/v3"
-)
-
-var (
-	__cron = &crontab{c: cron.New(cron.WithSeconds())}
 )
 
 type crontab struct {
 	c *cron.Cron
 }
 
-func Cron() *cron.Cron {
-	return __cron.c
+func New(opts ...cron.Option) *crontab {
+	return &crontab{c: cron.New(opts...)}
 }
 
-func Func(spec string, fn ...func()) *crontab {
-	for _, f := range fn {
-		__cron.c.AddFunc(spec, f)
-	}
-	return __cron
-}
-
-func Job(spec string, job ...cron.Job) *crontab {
-	for _, j := range job {
-		__cron.c.AddJob(spec, j)
-	}
-	return __cron
+func Default() *crontab {
+	return New(cron.WithSeconds())
 }
 
 func (c *crontab) Run() {
-	c.c.Run()
+	c.c.Start()
+
+	<-goo_context.Cancel().Done()
+	goo_log.WithTag("goo-cron").Debug("系统退出，等待全部任务执行结束...")
+
+	<-c.c.Stop().Done()
+	goo_log.WithTag("goo-cron").Debug("系统退出成功，全部任务执行结束")
 }
 
 func (c *crontab) Start() {
+	defer func() {
+		<-goo_context.Cancel().Done()
+		goo_log.WithTag("goo-cron").Debug("系统退出，等待全部任务执行结束...")
+
+		<-c.c.Stop().Done()
+		goo_log.WithTag("goo-cron").Debug("系统退出成功，全部任务执行结束")
+	}()
+
 	c.c.Start()
 }
 
@@ -43,14 +45,14 @@ func (c *crontab) Stop() {
 	c.c.Stop()
 }
 
-func (c *crontab) Func(spec string, fn ...func()) *crontab {
+func (c *crontab) AddFunc(spec string, fn ...func()) *crontab {
 	for _, f := range fn {
 		c.c.AddFunc(spec, f)
 	}
 	return c
 }
 
-func (c *crontab) Job(spec string, job ...cron.Job) *crontab {
+func (c *crontab) AddJob(spec string, job ...cron.Job) *crontab {
 	for _, j := range job {
 		c.c.AddJob(spec, j)
 	}
@@ -59,45 +61,45 @@ func (c *crontab) Job(spec string, job ...cron.Job) *crontab {
 
 // 每天0点0分0秒执行
 func (c *crontab) Day(fn ...func()) *crontab {
-	return __cron.Func("0 0 0 * * *", fn...)
+	return c.AddFunc("0 0 0 * * *", fn...)
 }
 
 // 每天x点0分0秒执行
 func (c *crontab) DayHour(hour int, fn ...func()) *crontab {
-	return __cron.Func(fmt.Sprintf("0 0 %d * * *", hour), fn...)
+	return c.AddFunc(fmt.Sprintf("0 0 %d * * *", hour), fn...)
 }
 
 // 每天x点x分0秒执行
 func (c *crontab) DayHourMinute(hour, minute int, fn ...func()) *crontab {
-	return __cron.Func(fmt.Sprintf("0 %d %d * * *", minute, hour), fn...)
+	return c.AddFunc(fmt.Sprintf("0 %d %d * * *", minute, hour), fn...)
 }
 
 // 每小时执行
 func (c *crontab) Hour(fn ...func()) *crontab {
-	return __cron.Func("0 0 */1 * * *", fn...)
+	return c.AddFunc("0 0 */1 * * *", fn...)
 }
 
 // 每隔x小时执行
 func (c *crontab) HourX(x int, fn ...func()) *crontab {
-	return __cron.Func(fmt.Sprintf("0 0 */%d * * *", x), fn...)
+	return c.AddFunc(fmt.Sprintf("0 0 */%d * * *", x), fn...)
 }
 
 // 每分钟执行
 func (c *crontab) Minute(fn ...func()) *crontab {
-	return __cron.Func("0 */1 * * * *", fn...)
+	return c.AddFunc("0 */1 * * * *", fn...)
 }
 
 // 每隔x分钟执行
 func (c *crontab) MinuteX(x int, fn ...func()) *crontab {
-	return __cron.Func(fmt.Sprintf("0 */%d * * * *", x), fn...)
+	return c.AddFunc(fmt.Sprintf("0 */%d * * * *", x), fn...)
 }
 
 // 每秒钟执行
 func (c *crontab) Second(fn ...func()) *crontab {
-	return __cron.Func("* * * * * *", fn...)
+	return c.AddFunc("* * * * * *", fn...)
 }
 
 // 每隔x秒执行
 func (c *crontab) SecondX(x int, fn ...func()) *crontab {
-	return __cron.Func(fmt.Sprintf("*/%d * * * * *", x), fn...)
+	return c.AddFunc(fmt.Sprintf("*/%d * * * * *", x), fn...)
 }
--- goo-cron/readme.md
+++ goo-cron/readme.md
@@ -1,13 +1,41 @@
 # 定时任务
 
+- 阻塞模式
+
 ```
-goo_cron.Minute(func() {
-    log.Println("Minute - 1")
-}, func() {
-    log.Println("Minute - 2")
-}).MinuteX(2, func() {
-    log.Println("MinuteX - 1")
-}, func() {
-    log.Println("MinuteX - 2")
-}).Start()
+c := goo_cron.Default()
+
+c.Second(func() {
+    fmt.Println("--11---", time.Now().Format("15:04:05"))
+    time.Sleep(3 * time.Second)
+    fmt.Println("--12---", time.Now().Format("15:04:05"))
+})
+c.SecondX(3, func() {
+    fmt.Println("--21---", time.Now().Format("15:04:05"))
+    time.Sleep(2 * time.Second)
+    fmt.Println("--22---", time.Now().Format("15:04:05"))
+})
+
+c.Run()
+```
+
+- 非阻塞模式
+
+```
+c := goo_cron.Default()
+
+c.Second(func() {
+    fmt.Println("--11---", time.Now().Format("15:04:05"))
+    time.Sleep(3 * time.Second)
+    fmt.Println("--12---", time.Now().Format("15:04:05"))
+})
+c.SecondX(3, func() {
+    fmt.Println("--21---", time.Now().Format("15:04:05"))
+    time.Sleep(2 * time.Second)
+    fmt.Println("--22---", time.Now().Format("15:04:05"))
+})
+
+c.Start()
+
+<-goo_context.Cancel().Done()
 ```
