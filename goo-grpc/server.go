ea1cce8bb8571b0b79b08b23c8416f40d31ecc7d
liqiongtao <hnatao@126.com>
2024-05-13 11:19:07 +0800

    bugfix

--- goo-grpc/server.go
+++ goo-grpc/server.go
@@ -35,8 +35,8 @@ func New(conf Config, opt ...ServerOption) *Server {
 	}
 
 	serverOptions := append(opts.ServerOptions, []grpc.ServerOption{
-		grpc.MaxRecvMsgSize(20 * 1024 * 1024),
-		grpc.MaxSendMsgSize(20 * 1024 * 1024),
+		grpc.MaxRecvMsgSize(100 * 1024 * 1024),
+		grpc.MaxSendMsgSize(100 * 1024 * 1024),
 		// 单向拦截 - 链式
 		grpc.ChainUnaryInterceptor(
 			serverUnaryInterceptorLog(),
