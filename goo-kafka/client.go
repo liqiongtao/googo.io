6362e53540ab93e7de26cac11e463853b77dff2c
liqiongtao <hnatao@126.com>
2024-06-24 14:23:03 +0800

    bugfix

--- goo-kafka/client.go
+++ goo-kafka/client.go
@@ -50,9 +50,9 @@ func (cli *client) init() (err error) {
 		sarama.NewBalanceStrategySticky(),
 		sarama.NewBalanceStrategyRange(),
 	}
-	config.Consumer.Group.Heartbeat.Interval = 600 * time.Second
-	config.Consumer.Group.Session.Timeout = 1800 * time.Second
-	config.Consumer.Group.Rebalance.Timeout = 1800 * time.Second
+	config.Consumer.Group.Heartbeat.Interval = 5 * time.Second
+	config.Consumer.Group.Session.Timeout = 15 * time.Second
+	config.Consumer.Group.Rebalance.Timeout = 12 * time.Second
 	if cli.conf.SessionTimeout > 0 {
 		config.Consumer.Group.Session.Timeout = time.Duration(cli.conf.SessionTimeout) * time.Second
 	}
