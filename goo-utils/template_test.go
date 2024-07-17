0ad01d946fd9cada3847a579ab6609c56ba2c534
liqiongtao <hnatao@126.com>
2024-07-17 16:05:56 +0800

    bugfix

--- goo-utils/template_test.go
+++ goo-utils/template_test.go
@@ -39,17 +39,17 @@ I like {{.Name}}
 
 func TestTemplate2(t *testing.T) {
 	m := M{
-		"enterpriseId": 66,
-		"name":         "123",
-		"ids":          []int64{1, 2, 3},
+		"enterpriseId": 63,
+		"name":         "hnatao",
+		"ids":          []int64{1, 2},
 	}
 
 	sqlstr := `
 SELECT * FROM u_user 
 	WHERE 1=1
-	{{if ne .enterpriseId 0}} and enterprise_id = {{.enterpriseId|args}} {{end}}
-	{{if ne (.ids|len) 0}} and id IN ({{.ids|args}}) {{end}}
-	{{if ne .name ""}} and name like {{.name|like|args}} {{end}}
+	{{if .enterpriseId}} and enterprise_id = {{.enterpriseId|args}} {{end}}
+	{{if .ids}} and id IN ({{.ids|args}}) {{end}}
+	{{if .name}} and name like {{.name|like|args}} {{end}}
 `
 	fmt.Println(Template(sqlstr, m))
 }
