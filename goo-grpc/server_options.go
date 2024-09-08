0ca7d0465654e5e3f747f7fe41b12707803328bb
liqiongtao <hnatao@126.com>
2024-09-08 19:18:14 +0800

    bufxi

--- goo-grpc/server_options.go
+++ goo-grpc/server_options.go
@@ -28,10 +28,10 @@ func newDefaultServerOptions(cfg Config) serverOptions {
 		Timeout: 20 * time.Second,
 	}
 	if cfg.KeepaliveTime > 0 {
-		keepaliveParams.Time = cfg.KeepaliveTime * time.Second
+		keepaliveParams.Time = cfg.KeepaliveTime
 	}
 	if cfg.KeepaliveTimeout > 0 {
-		keepaliveParams.Timeout = cfg.KeepaliveTimeout * time.Second
+		keepaliveParams.Timeout = cfg.KeepaliveTimeout
 	}
 
 	enforcementPolicy := keepalive.EnforcementPolicy{
@@ -44,7 +44,7 @@ func newDefaultServerOptions(cfg Config) serverOptions {
 		PermitWithoutStream: true,
 	}
 	if cfg.EnforcementPolicyMinTime > 0 {
-		enforcementPolicy.MinTime = cfg.EnforcementPolicyMinTime * time.Second
+		enforcementPolicy.MinTime = cfg.EnforcementPolicyMinTime
 	}
 
 	return serverOptions{
