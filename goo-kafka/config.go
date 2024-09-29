469469e200b4c1438aada75a76c021055cc72ea5
liqiongtao <hnatao@126.com>
2024-09-30 02:11:11 +0800

    bugfix

--- goo-kafka/config.go
+++ goo-kafka/config.go
@@ -3,12 +3,12 @@ package goo_kafka
 import goo_redis "github.com/liqiongtao/googo.io/goo-redis"
 
 type Config struct {
-	User              string   `json:"user" yaml:"user"`
-	Password          string   `json:"password" yaml:"password"`
-	Addrs             []string `json:"addrs" yaml:"addrs"`
-	Timeout           int      `json:"timeout" yaml:"timeout"`
-	HeartbeatInterval int      `json:"heartbeat_interval" yaml:"heartbeat_interval"`
-	SessionTimeout    int      `json:"session_timeout" yaml:"session_timeout"`
-	RebalanceTimeout  int      `json:"rebalance_timeout" yaml:"rebalance_timeout"`
-	RedisConfig       goo_redis.Config
+	User              string           `json:"user" yaml:"user"`
+	Password          string           `json:"password" yaml:"password"`
+	Addrs             []string         `json:"addrs" yaml:"addrs"`
+	Timeout           int              `json:"timeout" yaml:"timeout"`
+	HeartbeatInterval int              `json:"heartbeat_interval" yaml:"heartbeat_interval"`
+	SessionTimeout    int              `json:"session_timeout" yaml:"session_timeout"`
+	RebalanceTimeout  int              `json:"rebalance_timeout" yaml:"rebalance_timeout"`
+	RedisConfig       goo_redis.Config `json:"redis_config" yaml:"redis_config"`
 }
--- goo-kafka/option.go
+++ goo-kafka/option.go
@@ -1,5 +1,7 @@
 package goo_kafka
 
+import goo_redis "github.com/liqiongtao/googo.io/goo-redis"
+
 const (
 	FocusName = "focus"
 	RedisName = "redis"
@@ -13,4 +15,9 @@ type Option struct {
 // 是否强制
 func FocusOption() Option {
 	return Option{Name: FocusName, Value: true}
+}
+
+// redis
+func RedisOption(cli *goo_redis.Client) Option {
+	return Option{Name: RedisName, Value: cli}
 }
