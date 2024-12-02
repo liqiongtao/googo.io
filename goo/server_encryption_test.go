478dadcc5acebcb82f67ce7f2df09d2dbcc4e18d
liqiongtao <hnatao@126.com>
2024-12-02 15:23:49 +0800

    bugfix

--- goo/server_encryption_test.go
+++ goo/server_encryption_test.go
@@ -26,7 +26,7 @@ func TestEncryption_Decode(t *testing.T) {
 		Secret: "231a05f6faf619fb",
 	}
 
-	b, err := enc.Decode("940c2f8fea0d366ddaef4bc26661abeebf3bce75f7ee2effffcf02037467452a2e37766aec0a175375513655d023d4f7ada15625a282a0ae5f352586c4c57dad63b08ffaff05ee531cddbc724d26accd")
+	b, err := enc.Decode("")
 	if err != nil {
 		log.Println(err.Error())
 		return
