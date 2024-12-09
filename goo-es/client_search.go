8c464e20a9e5b6b739245206dcc2d646232af50b
liqiongtao <hnatao@126.com>
2024-12-09 16:38:29 +0800

    bugfix

--- goo-es/client_search.go
+++ goo-es/client_search.go
@@ -2,7 +2,12 @@ package goo_es
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"github.com/elastic/go-elasticsearch/v7/esapi"
+	goo_utils "github.com/liqiongtao/googo.io/goo-utils"
+	"io"
+	"time"
 )
 
 // 搜索
@@ -17,44 +22,131 @@ import (
 // - _source.excludes: 不返回字段
 //
 // GET /abd-*/_search
-// {
-//   "from" : 0,
-//   "size" : 10,
-//   "query": {
-//     "bool": {
-//       "must": [
-//         {"match": { "user_id": 2716 }},
-//         {"range": { "datetime": { "gte": "2024-01-14 00:00:00", "lte": "2024-01-14 23:59:59" }}},
-//         {"range": { "size": { "gte": 60, "lte": 3600 }}},
-//         {
-//           "nested": {
-//             "path": "list",
-//             "query": {
-//               "match_phrase": { "list.title": "青岛" }
-//             },
-//             "inner_hits": {
-//               "size": 1
-//             }
-//           }
-//         }
-//       ]
-//     }
-//   },
-//   "highlight": {
-//     "pre_tags": "<em>",
-//     "post_tags": "</em>",
-//     "fields": {
-//       "list.title": {}
-//     }
-//   },
-//   "_source": {
-//     "excludes" : ["list"]
-//   }
-// }
-func (c *ESClient) Search(index []string, b []byte) (*esapi.Response, error) {
+//
+//	{
+//	  "from" : 0,
+//	  "size" : 10,
+//	  "query": {
+//	    "bool": {
+//	      "must": [
+//	        {"match": { "user_id": 2716 }},
+//	        {"range": { "datetime": { "gte": "2024-01-14 00:00:00", "lte": "2024-01-14 23:59:59" }}},
+//	        {"range": { "size": { "gte": 60, "lte": 3600 }}},
+//	        {
+//	          "nested": {
+//	            "path": "list",
+//	            "query": {
+//	              "match_phrase": { "list.title": "青岛" }
+//	            },
+//	            "inner_hits": {
+//	              "size": 1
+//	            }
+//	          }
+//	        }
+//	      ]
+//	    }
+//	  },
+//	  "highlight": {
+//	    "pre_tags": "<em>",
+//	    "post_tags": "</em>",
+//	    "fields": {
+//	      "list.title": {}
+//	    }
+//	  },
+//	  "_source": {
+//	    "excludes" : ["list"]
+//	  }
+//	}
+func (c *ESClient) Search(index []string, body []byte) (*esapi.Response, error) {
 	req := esapi.SearchRequest{
 		Index: index,
-		Body:  bytes.NewReader(b),
+		Body:  bytes.NewReader(body),
 	}
 	return c.exec(req)
+}
+
+// 分页查询，用于大数量查询，普通查询，默认最多返回10000条
+func (c *ESClient) PageSearch(index []string, body []byte, fn func(p goo_utils.Params) error) error {
+	var (
+		ctx            = context.Background()
+		scrollDuration = 2 * time.Second
+		size           = 1000
+		scrollId       string
+	)
+
+	for n := 0; ; n++ {
+		var (
+			res *esapi.Response
+			err error
+		)
+
+		if n == 0 {
+			res, err = esapi.SearchRequest{
+				Index:  index,
+				Body:   bytes.NewReader(body),
+				Scroll: scrollDuration,
+				Size:   &size, // 每次获取的文档数量
+			}.Do(ctx, c.Client())
+		} else {
+			res, err = esapi.ScrollRequest{
+				ScrollID: scrollId,
+				Scroll:   scrollDuration,
+			}.Do(ctx, c.Client())
+		}
+
+		if err != nil {
+			c.log().Error(err)
+			return err
+		}
+
+		if res.IsError() {
+			c.log().Error(fmt.Errorf("error getting initial response: %s", res.String()))
+			return fmt.Errorf("error getting initial response: %s", res.String())
+		}
+
+		// 获取数据
+		b, err := io.ReadAll(res.Body)
+		if err != nil {
+			c.log().Error(err)
+			return err
+		}
+
+		// 关闭数据流
+		res.Body.Close()
+
+		// 转换
+		p, err := goo_utils.Byte(b).Params()
+		if err != nil {
+			c.log().Error(err)
+			return err
+		}
+
+		if len(p.Get("hits.hits").Array()) == 0 {
+			break
+		}
+
+		// 滚动ID
+		scrollId = p.Get("_scroll_id").String()
+
+		// 打印翻页
+		c.log().DebugF("第 %d 页，每页 %d 条", n+1, size)
+
+		// 处理文档
+		for _, hit := range p.Get("hits.hits").Array() {
+			if err = fn(hit.Get("_source")); err != nil {
+				c.log().Error(err)
+				return nil
+			}
+		}
+	}
+
+	// 清理Scroll上下文
+	_, err := esapi.ClearScrollRequest{
+		ScrollID: []string{scrollId},
+	}.Do(context.Background(), c.cli)
+	if err != nil {
+		c.log().Error(err)
+	}
+
+	return nil
 }
---
+++ goo-es/es_test.go
@@ -0,0 +1,44 @@
+package goo_es
+
+import (
+	goo_utils "github.com/liqiongtao/googo.io/goo-utils"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+	Init(Config{
+		Addresses: []string{""},
+		User:      "",
+		Password:  "",
+	})
+
+	date := "2024-12-08"
+
+	index := []string{"smartcard-http-sca-*"}
+
+	query := goo_utils.M{
+		"sort": []goo_utils.M{
+			{"log_datetime": "asc"},
+		},
+		"query": goo_utils.M{
+			"bool": goo_utils.M{
+				"filter": []goo_utils.M{
+					{"match_phrase": goo_utils.M{"log_tags": "/sca/device/reportinfo"}},
+					{
+						"range": goo_utils.M{
+							"log_datetime": goo_utils.M{
+								"gte": date + " 00:00:00",
+								"lte": date + " 23:59:59",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	Client().PageSearch(index, query.Json(), func(p goo_utils.Params) error {
+		//fmt.Println(p)
+		return nil
+	})
+}
