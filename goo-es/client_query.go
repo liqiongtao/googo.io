2030c7fd38aa03915ccfe6b879ee6c0a8cbcaace
liqiongtao <hnatao@126.com>
2024-12-09 10:45:03 +0800

    bugfix

--- goo-es/client_query.go
+++ goo-es/client_query.go
@@ -60,3 +60,47 @@ func (c *ESClient) Query(index []string, filter []goo_utils.M, offset, size int)
 
 	return p.Get("hits.total.value").Int64(), p.Get("hits.hits").Array()
 }
+
+func (c *ESClient) LoopV2(index []string, m goo_utils.M, fn func(p goo_utils.Params) error) {
+	size := 5000
+	for n := 0; n < 100; n++ {
+		_, list := c.QueryV2(index, m)
+		l := len(list)
+
+		goo_log.Debug("[ES]", "查询数量", l, n, n*size, size)
+
+		if l == 0 {
+			return
+		}
+
+		for _, i := range list {
+			if err := fn(i); err != nil {
+				goo_log.Error("[ES]", err, i)
+				return
+			}
+		}
+	}
+}
+
+func (c *ESClient) QueryV2(index []string, m goo_utils.M) (int64, []goo_utils.Params) {
+	res, err := c.Search(index, m.Json())
+	if err != nil {
+		goo_log.Error("[ES]", err)
+		return 0, []goo_utils.Params{}
+	}
+	defer res.Body.Close()
+
+	var b bytes.Buffer
+	if _, er := io.Copy(&b, res.Body); er != nil {
+		goo_log.Error("[ES]", er.Error())
+		return 0, []goo_utils.Params{}
+	}
+
+	p, er := goo_utils.Byte(b.Bytes()).Params()
+	if er != nil {
+		goo_log.Error("[ES]", er.Error())
+		return 0, []goo_utils.Params{}
+	}
+
+	return p.Get("hits.total.value").Int64(), p.Get("hits.hits").Array()
+}
