5af5c47029c149c8db744763bcc8636b9cb6968c
liqiongtao <hnatao@126.com>
2024-12-13 16:09:41 +0800

    bugfix

--- goo-message/feishu.go
+++ goo-message/feishu.go
@@ -13,18 +13,41 @@ import (
 var (
 	__fieShuCH   chan struct{}
 	__feiShuOnce sync.Once
+	__feiShuMu   sync.Mutex
+	__feiShuUniq = map[string]any{}
 )
 
 func FeiShu(hookUrl string, text string) error {
+	if text == "" {
+		return nil
+	}
+
 	__feiShuOnce.Do(func() {
 		__fieShuCH = make(chan struct{}, runtime.NumCPU()*2)
 	})
 
+	// 控制并发
 	__fieShuCH <- struct{}{}
 	defer func() { <-__fieShuCH }()
 
-	if text == "" {
-		return nil
+	// 去重
+	{
+		key := goo_utils.MD5([]byte(text))
+
+		__feiShuMu.Lock()
+		if _, ok := __feiShuUniq[key]; ok {
+			__feiShuMu.Unlock()
+			return nil
+		}
+
+		__feiShuUniq[key] = struct{}{}
+		__feiShuMu.Unlock()
+
+		defer func() {
+			__feiShuMu.Lock()
+			delete(__feiShuUniq, key)
+			__feiShuMu.Unlock()
+		}()
 	}
 
 	data := map[string]interface{}{
