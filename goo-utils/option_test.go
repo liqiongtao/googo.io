3e1c4d0202cb8e03b308fb406aaeb73eccbc0f63
liqiongtao <hnatao@126.com>
2025-09-21 18:47:11 +0800

    bugfix

--- goo-utils/option_test.go
+++ goo-utils/option_test.go
@@ -17,13 +17,33 @@ func NewPerson(opts ...Option[*Person]) *Person {
 	return u
 }
 
-func WithName(name string) Option[*Person] {
-	return func(u *Person) {
-		u.Name = name
+type Member struct {
+	Name string
+}
+
+func NewMember(opts ...Option[*Member]) *Member {
+	u := &Member{}
+	for _, opt := range opts {
+		opt.Apply(u)
+	}
+	return u
+}
+
+func WithName[T any](name string) Option[T] {
+	return func(u T) {
+		if p, ok := any(u).(*Person); ok {
+			p.Name = name
+		}
+		if m, ok := any(u).(*Member); ok {
+			m.Name = name
+		}
 	}
 }
 
 func TestNewPerson(t *testing.T) {
-	u := NewPerson(WithName("liqiongtao"))
+	u := NewPerson(WithName[*Person]("liqiongtao"))
 	fmt.Println(u.Name)
+
+	m := NewMember(WithName[*Member]("liqiongtao"))
+	fmt.Println(m.Name)
 }
