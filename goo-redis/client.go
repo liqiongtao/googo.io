0fa5b201f73e8e187ecc9f57cb97e2e750a35cbc
liqiongtao <hnatao@126.com>
2026-02-24 16:22:54 +0800

    bugfix

--- goo-redis/client.go
+++ goo-redis/client.go
@@ -1,6 +1,8 @@
 package goo_redis
 
 import (
+	"time"
+
 	"github.com/go-redis/redis"
 	goo_cron "github.com/liqiongtao/googo.io/goo-cron"
 	goo_log "github.com/liqiongtao/googo.io/goo-log"
@@ -14,24 +16,22 @@ type Client struct {
 func New(conf Config) (cli *Client, err error) {
 	cli = &Client{Config: conf}
 
-	var opts *redis.Options
-	if conf.Options != nil {
-		opts = conf.Options
-	} else {
-		opts = DefaultOptions
-	}
+	cli.Client = redis.NewClient(&redis.Options{
+		Addr:     conf.Addr,
+		Password: conf.Password,
+		DB:       conf.DB,
 
-	if conf.Addr != "" {
-		opts.Addr = conf.Addr
-	}
-	if conf.Password != "" {
-		opts.Password = conf.Password
-	}
-	if conf.DB != 0 {
-		opts.DB = conf.DB
-	}
+		// 连接池
+		PoolSize:     20,
+		MinIdleConns: 5,
+		PoolTimeout:  30 * time.Second,
+		IdleTimeout:  5 * time.Minute,
 
-	cli.Client = redis.NewClient(opts)
+		// 超时
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second, // 调大读超时
+		WriteTimeout: 10 * time.Second,
+	})
 
 	if err = cli.Ping().Err(); err != nil {
 		goo_log.WithTag("goo-redis").Error(err)
