fd3ad4fd37aae2fc1ce97fdd31af369e81113528
liqiongtao <hnatao@126.com>
2026-02-26 12:34:35 +0800

    bugfix

--- goo-utils/gopsutil.go
+++ goo-utils/gopsutil.go
@@ -2,6 +2,7 @@ package goo_utils
 
 import (
 	goo_log "github.com/liqiongtao/googo.io/goo-log"
+	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
@@ -13,4 +14,24 @@ func MemoryUsedPercent() (float64, error) {
 		return 0, err
 	}
 	return v.UsedPercent, nil
+}
+
+// 内存
+func Memory() (float64, float64, float64, error) {
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		goo_log.Error(err)
+		return 0, 0, 0, err
+	}
+	return float64(v.Total) / 1024 / 1024 / 1024, float64(v.Used) / 1024 / 1024 / 1024, float64(v.Available) / 1024 / 1024 / 1024, nil
+}
+
+// 空间
+func Disk() (float64, float64, float64, error) {
+	v, err := disk.Usage("/")
+	if err != nil {
+		goo_log.Error(err)
+		return 0, 0, 0, err
+	}
+	return float64(v.Total) / 1024 / 1024 / 1024, float64(v.Used) / 1024 / 1024 / 1024, float64(v.Free) / 1024 / 1024 / 1024, nil
 }
