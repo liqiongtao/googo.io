fe3ff8ac5eb8bf5ff473571394457d1b7eb32173
liqiongtao <hnatao@126.com>
2026-03-25 19:35:26 +0800

    bugfix

--- goo-xlsx/xlsx_writer_test.go
+++ goo-xlsx/xlsx_writer_test.go
@@ -10,7 +10,7 @@ func TestXlsxWrite_Handler(t *testing.T) {
 
 	w.SetSheetName("数据导出")
 
-	w.SetMergeCellValue("A1", "D2", "大标题1")
+	w.SetMergeCellValue("A1", "D2", "大标题1", defaultCellStyle)
 
 	w.SetRowNum(3)
 	w.SetMergeCellValue("A3", "B3", "子标题1")
@@ -18,8 +18,8 @@ func TestXlsxWrite_Handler(t *testing.T) {
 
 	left, right, err := w.SetTitles([]string{"姓名", "手机号"})
 	fmt.Println(left, right, err)
-	
-	w.SetData([]interface{}{"李涛", "18510381580"})
+
+	w.SetData([]interface{}{"李涛", "18510381580"}, defaultTitleStyle)
 
 	w.Save2File("user.xlsx")
 }
