a4e1d580fc58378e6b0c06b703b418ab256bddb7
liqiongtao <hnatao@126.com>
2026-04-01 10:23:50 +0800

    bugfix

--- goo-cron/publish.go
+++ goo-cron/publish.go
@@ -2,8 +2,11 @@ package goo_cron
 
 import (
 	"github.com/go-redis/redis"
+	goo_log "github.com/liqiongtao/googo.io/goo-log"
 )
 
 func Publish(r *redis.Client, key string, task *TaskData) error {
-	return r.Publish(key, task.String()).Err()
+	payload := task.String()
+	goo_log.WithField("payload", payload).Info("发布任务")
+	return r.Publish(key, payload).Err()
 }
