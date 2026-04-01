a0236140e3e6e297c7f899191e97410d1df704bb
liqiongtao <hnatao@126.com>
2026-04-01 09:37:32 +0800

    bufix

--- goo-cron/publish.go
+++ goo-cron/publish.go
@@ -4,6 +4,6 @@ import (
 	"github.com/go-redis/redis"
 )
 
-func Publish(r *redis.Client, key string, task TaskData) error {
+func Publish(r *redis.Client, key string, task *TaskData) error {
 	return r.Publish(key, task.String()).Err()
 }
