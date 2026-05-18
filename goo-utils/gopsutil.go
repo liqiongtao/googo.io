b99bcfaf0d28cea87e8db1ecede1b8ddae3baab8
liqiongtao <hnatao@126.com>
2026-05-18 17:16:07 +0800

    bufix

--- goo-utils/gopsutil.go
+++ goo-utils/gopsutil.go
@@ -6,38 +6,42 @@ import (
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
-// 内存占比
+// MemoryUsedPercent 内存占比
 func MemoryUsedPercent() (float64, error) {
 	v, err := mem.VirtualMemory()
 	if err != nil {
 		goo_log.Error(err)
 		return 0, err
 	}
-	return v.UsedPercent, nil
+	return 100 - float64(v.Available)/float64(v.Total)*100, nil
 }
 
-// 内存
-func Memory() (float64, float64, float64, error) {
+// Memory 总内存 已用内存 可用内存 使用率
+func Memory() (float64, float64, float64, float64, error) {
 	v, err := mem.VirtualMemory()
 	if err != nil {
 		goo_log.Error(err)
-		return 0, 0, 0, err
+		return 0, 0, 0, 0, err
 	}
-	return float64(v.Total) / 1024 / 1024 / 1024,
-		float64(v.Used) / 1024 / 1024 / 1024,
-		float64(v.Available) / 1024 / 1024 / 1024,
+	const gb = 1024 * 1024 * 1024
+	return float64(v.Total) / gb,
+		float64(v.Total-v.Available) / gb,
+		float64(v.Available) / gb,
+		100 - float64(v.Available)/float64(v.Total)*100,
 		nil
 }
 
-// 空间
-func Disk() (float64, float64, float64, error) {
+// Disk 总大小 已用大小 可用大小 使用率
+func Disk() (float64, float64, float64, float64, error) {
 	v, err := disk.Usage("/")
 	if err != nil {
 		goo_log.Error(err)
-		return 0, 0, 0, err
+		return 0, 0, 0, 0, err
 	}
-	return float64(v.Total) / 1024 / 1024 / 1024,
-		float64(v.Used) / 1024 / 1024 / 1024,
-		float64(v.Free) / 1024 / 1024 / 1024,
+	const gb = 1024 * 1024 * 1024
+	return float64(v.Total) / gb,
+		float64(v.Used) / gb,
+		float64(v.Free) / gb,
+		v.UsedPercent,
 		nil
 }
